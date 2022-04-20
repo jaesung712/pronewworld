975c6369e6cb6c548b4cb79455125973d2637164
jaesung <jaesung190@gmail.com>
2022-04-20 23:25:47 +0900

    20220420

--- src/user/component/formain.tsx/forpage.tsx
+++ src/user/component/formain.tsx/forpage.tsx
@@ -1,11 +1,28 @@
 import React, { useState, Component } from "react";
 import styled from "styled-components";
 const Forpagest = styled.div`
+    p{
+        margin:0;
+    }
+    .container{
+        width:100%;
+        background-color: white;
+        margin-top:1.8em;
+        border: 1px black;
+        height:100vh;
+        .contentbox{
+            margin: 40px;
+        }
+    }
 `
 const forpage = (): JSX.Element => {
     return(
         <Forpagest>
-            
+            <div className="container">
+                <div className="contentbox">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium temporibus atque perferendis quia nihil libero quae quos, dolore cumque ipsam expedita incidunt sapiente fuga consequuntur numquam! Atque deleniti earum qui!</p>
+                </div>
+            </div>
         </Forpagest>
     )
 }
--- src/user/main.tsx
+++ src/user/main.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 import Sidebar from "./component/header/sidebar";
 import Miniheader from "./component/header/miniheader";
+import Forpage from "./component/formain.tsx/forpage";
+
+const Mainst = styled.div`
+    .container{
+        display: flex;
+    }
+    .rigth{
+        padding-left: 20rem;
+    }
+`
 const main = (): JSX.Element =>{
     return(
-        <div className="container">
-            <Sidebar></Sidebar>
-            <Miniheader></Miniheader>
-        </div>
+        <Mainst>
+            <div className="container">
+                <div className="left">
+                <Sidebar></Sidebar>
+                </div>
+                <div className="rigth">
+                    <Miniheader></Miniheader>
+                    <Forpage></Forpage>
+                </div>
+                
+            </div>
+        </Mainst>
     )
 }
 
