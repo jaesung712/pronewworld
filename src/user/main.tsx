8f299495ece6d3e26eaed14457103503d3f750b7
jaesung <jaesung190@gmail.com>
2022-05-07 07:07:06 +0900

    20220507 PopToMain

--- src/user/main.tsx
+++ src/user/main.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { SetStateAction, useEffect, useState ,useRef, FunctionComponent } from "react";
 import styled from "styled-components";
 import Sidebar from "./component/header/sidebar";
 import Miniheader from "./component/header/miniheader";
 import Forpage from "./component/formain.tsx/forpage";
+import Spt1 from "./component/popup/sidepopt1";
+import Headerpop from "./component/popup/headerpop";
 
 const Mainst = styled.div`
     .container{
@@ -12,15 +14,44 @@ const Mainst = styled.div`
         padding-left: 20rem;
     }
 `
-const main = (): JSX.Element =>{
+const Main = (): JSX.Element =>{
+    const[popupon, setpopupon] = React.useState(false);
+    const [moredis,setmoredis] = useState(false)
+
+    const onpopup = () => {
+        setpopupon(!popupon);
+        if(popupon===true){
+            document.body.style.overflow = "hidden";
+            
+        }else if(popupon===false){
+            document.body.style.overflowY = "unset";
+            //만약 헤더더보기가 켜져있다면?
+            if(moredis ===true){
+                setmoredis(!moredis)
+            }
+			
+        }
+    }
+    //사이드바 설정버튼 제어
+	const Sidepop = () =>{
+		return(
+			<Spt1 onpopup={onpopup}></Spt1>
+		)
+	}
+    //헤더 더보기 버튼 제어
+    const showmore = ()=>{
+        setmoredis(!moredis);
+}
     return(
         <Mainst>
             <div className="container">
                 <div className="left">
-                <Sidebar></Sidebar>
+                <Sidebar onpopup={onpopup}></Sidebar>
+                {popupon?<Sidepop/>:''}
                 </div>
                 <div className="rigth">
-                    <Miniheader></Miniheader>
+                    <Miniheader showmore={showmore}></Miniheader>
+                    {moredis?<Headerpop/>:''}
                     <Forpage></Forpage>
                 </div>
                 
@@ -29,5 +60,9 @@ const main = (): JSX.Element =>{
     )
 }
 
-export default  main;
+export default  Main;
+
+
+
+
 
